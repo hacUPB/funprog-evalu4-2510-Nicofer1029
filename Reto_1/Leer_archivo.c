430e92150eaaa0483cace3566617803b891d44bd
Nicolás Fernández Barreto <nicolas.fernandezb@upb.edu.co>
2025-05-15 08:13:26 -0500

    Avances reto 1

---
+++ Reto_1/Leer_archivo.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char nombreArchivo[100];
+    FILE *archivo;
+    char *contenido;
+    long tamano;
+
+    printf("Ingrese el nombre del archivo .txt a leer: ");
+    scanf("%s", nombreArchivo); 
+
+    archivo = fopen(nombreArchivo, "r");
+    if (archivo == NULL) {
+        perror("Error al abrir el archivo");
+        return 1;
+    }
+
+    contenido = (char *)malloc((tamano + 1) * sizeof(char));
+    if (contenido == NULL) {
+        perror("No se pudo asignar memoria");
+        fclose(archivo);
+        return 1;
+    }
+
+    
+    fread(contenido, sizeof(char), tamano, archivo);
+    contenido[tamano];
+
+    printf("\n Contenido del archivo:\n%s\n", contenido);
+
+    free(contenido);
+    fclose(archivo);
+
+    return 0;
+}
